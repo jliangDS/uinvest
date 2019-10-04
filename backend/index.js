afa85c7a71d35b00135d7ea387b4fdb4c2e7dddf
Jun Liang <illuminatemynight@gmail.com>
2019-10-04 02:14:39 -0500

    added knex

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,11 @@
-const express = require('express')
+const databaseSettings = require('./knexfile.js').development
+const knex = require('knex')(databaseSettings)
+const sqlite = require('sqlite3')
 
+const express = require('express')
+const cors = require('cors')
 const app = express()
+const bodyParser = require('body-parser')
 
 app.get('/hi', (request, response) => {
     response.json({
